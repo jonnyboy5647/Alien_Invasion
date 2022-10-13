8b0b846242067319e4a1ffaab95dbe34c1ca6c67
jonnyboy5647 <m253546@usna.edu>
2022-10-13 09:42:42 -0400

    Finish Lab 7

--- settings.py
+++ settings.py
@@ -5,4 +5,4 @@ class Settings:
         """Initilize the game's settings"""
         self.screen_width = 1000
         self.screen_height = 600
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (230, 0, 0)
